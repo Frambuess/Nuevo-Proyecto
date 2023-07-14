8ed03a2a7606e7e427c98da5d583a273e29a9621
Irina Moldavsky <frambuess@gmail.com>
2023-07-14 13:53:38 -0300

    add: modelo Estudiante, Profesor y Curso

--- project/apps/CORE/models.py
+++ project/apps/CORE/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    especialidad = models.CharField(max_length=100)
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
