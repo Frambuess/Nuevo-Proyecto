76165e0f8da85142a012a1ed934c0a98b165d5c4
Irina Moldavsky <frambuess@gmail.com>
2023-07-15 18:56:40 -0300

    ult de las busquedas

--- project/apps/CORE/views.py
+++ project/apps/CORE/views.py
@@ -34,14 +34,14 @@ def busqueda(request: HttpRequest) -> HttpResponse:
     cliente_nombre = Cliente.objects.filter(nombre__contains="M")
 
     # Nacimientos  mayores a 2005
-    cliente_nacimiento = Cliente.objects.filter( nacimiento__gt=date(2005, 1, 1))
+    cliente_nacimiento = Cliente.objects.filter( nacimiento__lt=date(2005, 1, 1))
 
     #age = date.today().year - nacimiento.year - ((date.today().month, date.today().day) < (nacimiento.month, nacimiento.day))
 
     #cliente_nacimiento = Cliente.objects.filter((date.today().year - nacimiento.year - ((date.today().month, date.today().day) < (nacimiento.month, nacimiento.day)))__gte=18)
 
     # País de origen vacío
-    cliente_pais = Cliente.objects.filter(pais_origen_id="Argentina")
+    cliente_pais = Cliente.objects.filter(pais_origen_id=1)
 
     contexto = {
         "clientes_nombre": cliente_nombre,
