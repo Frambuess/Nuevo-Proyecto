ce38f10557efb8630ed85274d2493007d2d71c3c
Irina Moldavsky <frambuess@gmail.com>
2023-07-15 12:09:31 -0300

    cambios nombres medio

--- project/apps/CORE/models.py
+++ project/apps/CORE/models.py
@@ -9,18 +9,18 @@ class Pais(models.Model):
         return self.nombre
 
 class Mediodepago(models.Model):
-    marca=models.CharField(max_length=25)
+    medio=models.CharField(max_length=25)
 
     def __str__(self):
-        return self.marca
+        return self.medio
 
 class Cliente(models.Model):
     pais_origen_id=models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True)
     apellido=models.CharField(max_length=50)
     nombre=models.CharField(max_length=50)
     nacimiento= models.DateField(null=True) # opcional
-    marca_id=models.ForeignKey(Mediodepago, on_delete=models.SET_NULL, null=True)
+    medio_id=models.ForeignKey(Mediodepago, on_delete=models.SET_NULL, null=True)
     nrotarjeta=models.IntegerField(null=True)
 
     def __str__(self):
-        return f"{self.nombre} {self.apellido}. Nació el {self.nacimiento}. Su tarjeta es {self.marca_id} nro {self.nrotarjeta}"
+        return f"{self.nombre} {self.apellido}. Nació el {self.nacimiento}. Su tarjeta es {self.medio_id} nro {self.nrotarjeta}"
